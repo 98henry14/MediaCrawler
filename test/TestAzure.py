af6380b28df750952cb033d0fc706830c9e2ce5a
xiaoj <xie.xj.17@pg.com>
2024-09-11 17:24:16 +0800

    1爬取新东方的m3u8文件并解密合并的脚本

--- test/TestAzure.py
+++ test/TestAzure.py
@@ -1,17 +1,31 @@
+import base64
+import datetime
 import subprocess
+import threading
+
 import requests
-import urllib
+from urllib.parse import urlparse, parse_qs
 
+import json
 import os
+# import pycryptodome
+from Crypto.Cipher import AES
+from concurrent.futures import ThreadPoolExecutor,as_completed
 
+dir_path="C:\\Users\\xiaoj\\Downloads"
+old_m3u8_path=f"{dir_path}\\ceshi.m3u8"
+ts_path=f"{dir_path}\\m3u8"
+new_index_m3u8="index-2.m3u8"
+new_m3u8_path=f"{ts_path}\\{new_index_m3u8}"
+merge_video_name="hello.mp4"
 
-# def run_azure_command(command):
-#     try:
-#         result = subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#         return result.stdout.strip()
-#     except subprocess.CalledProcessError as e:
-#         print(f"Error running command: {e}")
-#         return None
+def run_azure_command(command):
+    try:
+        result = subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command: {e}")
+        return None
 # r1 = subprocess.run("az --version", check=True, shell=True, stdout=subprocess.PIPE,  text=True)
 # if r1:
 #     print(r1.stdout.strip())
@@ -24,19 +38,42 @@ import os
 #     print("执行aure")
 #     print(res.split("\n"))
 
+
+
 # # 从本地读取m3u8文件，获取ts连接并下载
 def read_m3u8_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     ts_urls = []
+    key_url = ""
+    # 同时写入新的文件里面
+    index = open(new_m3u8_path, "w")
     for line in lines:
+
         if line.startswith('#'):
+            if line.startswith('#EXT-X-KEY:'):
+                key_url = line.split(',')[1].strip().replace("URI=", "").replace("\"", "")
+            else:
+                index.write(line)
             continue
-
+        index.write(f"{ts_path}\\{line.split('?')[0]}\n")
         ts_urls.append(line.strip())
-    return ts_urls
+    return key_url,ts_urls
 
-res = read_m3u8_file("C:\\Users\\xiaoj\\Downloads\\7fe74586e09c45a8a65e45ea0e2ec19c.m3u8")
+def decrypt_key(raw_content,token):
+    print(raw_content)
+
+    tk = json.loads(base64.b64decode(token))
+    print(tk)
+    key = tk['key']
+    newR = []
+    for d in dt:
+        newR.append(d ^ key)
+    return bytes(newR)
+
+
+# 从本地读取m3u8文件，并提取
+kurl,res = read_m3u8_file(old_m3u8_path)
 print(res)
 
 
@@ -64,30 +101,98 @@ params = {
     "auth_key": "1726318708-d837203db925427b8576e8adec33e6d7-0-fa96b7c010b86635fc89962135d54425"
 }
 
-# with open("C:\\Users\\xiaoj\\Downloads\\m3u8\\decrypt",mode="wb") as f:
-#     url="https://media-vod.roombox.xdf.cn/v1/play/decrypt"
-#     params = {
-#         "Ciphertext": "N2FiYzViMmUtZjJhNi00YzdhLThjYWUtZjY3Yzk2MWZmZmJmZU9uand6VUdGTHhPd1lYb2IrUEFGTnJCMytHWEpFNnZBQUFBQUFBQUFBRFBkaXN3aWQwVjQ5Qy84cG04cTRJZXZGcTJxR2hVWTFnOGlaNDVZOWVqY0xSU2tpZDl2UmIx",
-#         "vid": "5bf82ce0eda271ee9452f6e7d7696302",
-#         "KeyServiceType": "KMS",
-#         "FromService": "Roombox",
-#         "MtsHlsUriToken": "eyJ0aW1lIjoxNzI2MDM2MDEzLCJ0b2tlbiI6ImM2ZDkwYzY1MTg2NTc0OTBiYjNkYWRkYjMwOWMzNzE4Iiwia2V5Ijo2MywiZW5jX3R5cGUiOjIsInVybF90eXBlIjoxLCJkZWZpbml0aW9uIjoiU0QiLCJzdHJlYW1fdHlwZSI6InZpZGVvIiwiYXBwX2lkIjoia29vbGVhcm5fMTAwMjAwMSIsIkNhdGVnb3J5SWQiOjN9"
-#     }
-#     resp = requests.get(url, headers=headers, params=params , verify=False)
-#     f.write(resp.content)
 
-with open("C:\\Users\\xiaoj\\Downloads\\m3u8\\file2.txt", mode="w") as f:
-    for re in res:
-        url =f"https://media-editor.roombox.xdf.cn/clouddriver-transcode/5bf82ce0eda271ee9452f6e7d7696302/{re}"
-        urlsplit = re.split("?")
-        # response = requests.get(url, headers=headers)
-        # print(response)
-        # with open(os.path.join("C:\\Users\\xiaoj\\Downloads\\", urlsplit[0]),
-        #           mode="wb") as f3:
-        #     f3.write(response.content)
-        print("file './{}'\n".format( urlsplit[0]))
-        f.write("file './{}'\n".format( urlsplit[0]))
-    # print(response.text)
+# 解析key的 URL
+parsed_url = urlparse(kurl)
 
-ts_path="C:\\Users\\xiaoj\\Downloads\\m3u8\\"
-os.system(f"ffmpeg -f concat -safe 0 -i {ts_path}file.txt -c copy {ts_path}video.mp4")
+# 提取查询参数
+query_params = parse_qs(parsed_url.query)
+
+# 打印提取的参数
+print("原始URL:", kurl)
+print("解析后的URL:", parsed_url)
+print("查询参数:", query_params)
+vid = query_params['vid'][0]
+
+# 提取key，并获取最终解密的key
+resp = requests.get(kurl, headers=headers,  stream=True)
+resp.raw.decode_content = True
+dt = resp.raw.read()
+kb = decrypt_key(dt,query_params['MtsHlsUriToken'][0])
+# okb = [59,223,20,27,102,158,210,1,199,119,114,66,238,119,185,46]
+# kb = bytes(okb)
+print(kb)
+ib = bytes([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+
+"""
+iv 的加密方式如下，问题 A 从哪里来，貌似都回累计加1，
+A.prototype.createInitializationVector = function(A) {
+    for (var t = new Uint8Array(16), e = 12; e < 16; e++)
+        t[e] = A >> 8 * (15 - e) & 255;
+    return t
+}
+
+key的取值
+1.从m3u8中获取到加密地址uri，调用 并获得二进制数组,byteArr
+2.提取uri中MtsHlsUriToken的值，通过base64解析为json，再提取其中key
+3.将获得二进制数组byteArr逐位跟key进行 ^ 异或操作，最终结果就是kb
+
+        
+"""
+def download_ts(re):
+    url = f"https://media-editor.roombox.xdf.cn/clouddriver-transcode/{vid}/{re}"
+    urlsplit = re.split("?")
+    response = requests.get(url, headers=headers)
+    # print(response)
+    desc = AES.new(kb, AES.MODE_CBC, ib).decrypt(response.content)
+    with open(os.path.join(ts_path+"\\", urlsplit[0]),
+              mode="wb") as f3:
+        f3.write(desc)
+    print("{}->file './{}'".format(threading.currentThread().getName(),urlsplit[0]))
+    return True
+
+start= datetime.datetime.now()
+task= []
+# print("使用多线程方式下载ts文件")
+# with ThreadPoolExecutor(max_workers=50) as executor:
+#     # results = executor.map(download_ts,res)
+#     task.extend([executor.submit(download_ts, ts) for ts in res])
+#     print("任务总数",len(task))
+#
+# as_completed(task,timeout=60)
+# print("使用多线程方式下载ts文件完成,耗时",datetime.datetime.now()-start)
+# 任务总数 23,使用多线程方式下载ts文件完成,耗时 0:01:06.389875
+
+# 如果不使用多线程执行的话
+print("循环下载ts文件")
+for re in res:
+    download_ts(re)
+print("循环下载ts文件完成,耗时",datetime.datetime.now()-start)
+# 任务总数 23 循环下载ts文件完成,耗时 0:00:48.333951
+
+
+# os.chdir(dir_path+"m3u8")
+# os.system("ffmpeg -i index.m3u8 -c copy new1.mp4")
+run_azure_command(f"cd {ts_path}")
+run_azure_command(f"ffmpeg -i {new_index_m3u8} -c copy {merge_video_name}")
+# import csv
+#
+# # res = os.system("az ad user list --filter \"userPrincipalName eq 'xie.xj.17@pg.com'\" --query [].userPrincipalName --output tsv")
+#
+# def run_azure_command(command):
+#     try:
+#         result = subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+#         return result.stdout.strip()
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error running command: {e}")
+#         return None
+# res = run_azure_command("az ad user list --filter \"userPrincipalName eq 'xie.xj.17@pg.com' or userPrincipalName eq 'chen.x.100@pg.com'\" --query [].userPrincipalName --output tsv")
+# with open("C:\\Users\\xiaoj\\Desktop\\usercheck\\ad_users-3.csv", mode="w",newline='',encoding="utf-8") as ff:
+#     write = csv.writer(ff)
+#     # print(res)
+#     print(res.split("\n"))
+#     print([[line] for line in res.split("\n")])
+#     # for a in res.split("\n"):
+#     #     write.writerow([a])
+#     # write.writerow(res)
+#     write.writerows([[line] for line in res.split("\n")])
